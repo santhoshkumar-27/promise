2872d8c85638547c6d7c44247a6949ed8c29436a
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-11-17 11:29:16 +0530

    for promise

---
+++ index.js
@@ -0,0 +1,10 @@
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(reject, 100, '500');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log('values' ,values);
+}).catch((err) => console.log('err',err));
+// expected output: Array [3, 42, "foo"]
