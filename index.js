9b5bd75b801f248639bbd2fd75b1d3a905c1dd37
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-11-17 12:36:39 +0530

    studied the promise.all

--- index.js
+++ index.js
@@ -16,3 +16,37 @@ Promise.all([promise1, promise2, promise3]).then((values) => {
     suppose it will get any reject any one of these
     it will go to the catch block
 */
+
+
+// Promise.allsettled
+const promise4 = Promise.resolve(3);
+const promise5 = 42;
+const promise6 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 500);
+    // setTimeout(reject, 100, '500');
+});
+
+Promise.allSettled([promise4, promise5, promise6])
+.then((results) => console.log(results))
+.catch((err) => console.log(err));
+
+
+/*
+    Promise.allSettled() takes a array of promise it give only one output promise array of settled outpu
+    And it give a array of object which consist of the property status, value or reason to the respective promise
+*/
+
+
+/**
+ * @param
+ * @returns
+ */ 
+
+/**
+ * @param  {} firstName
+ * @param  {} lastName
+ * @returns Full name combined by concat with firstName + lastName
+ */
+function name(firstName, lastName) {
+    return firstName + lastName;
+}
