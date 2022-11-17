815fee422af04b430b737c0db739c8ba537a733c
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-11-17 11:53:31 +0530

    studied promise.all

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const promise1 = Promise.resolve(3);
 const promise2 = 42;
 const promise3 = new Promise((resolve, reject) => {
-  setTimeout(reject, 100, '500');
+    // setTimeout(resolve, 100, '500');
+    setTimeout(reject, 100, '500');
 });
 
 Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log('values' ,values);
-}).catch((err) => console.log('err',err));
+    console.log('values', values);
+}).catch((err) => console.log('error', err));
 // expected output: Array [3, 42, "foo"]
+
+/*
+    Promise.all() takes a array of promise it give only one output promise
+    And it give result only all promise get resolve, it will go to the resolve block
+    suppose it will get any reject any one of these
+    it will go to the catch block
+*/
